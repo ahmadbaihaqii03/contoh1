365dbcd24c3e33cbfb3c1dad3dd4e671415f8ae1
ahmadbaihaqii03 <ahmadbaihaqii187@gmail.com>
2022-04-14 11:51:37 +0700

    menambahkan file tugas

---
+++ application/views/v_header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Web Program II | Merancang Template sederhana dengan 
+        Codeigniter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo
+    base_url() ?>assets/css/stylebuku.css">
+</head>
+<body>
+<div id="wrapper">
+    <header>
+        <hgrup>
+            <h1>RentalBuku.net</h1>
+            <h3>Membuat Template Sederhana dengan
+                CodeIgniter</h3>
+        </hgrup>
+        <nav>
+            <ul>
+                <li><a href="<?php echo base_url(). 'index.php/web' ?>">Home</a>
+</li>
+               <li><a href="<?php echo base_url(). 'index.php/web/about' ?>">About</a>
+</li>
+            </ul>
+        </nav>
+<div class="clear"></div>
+</header>
